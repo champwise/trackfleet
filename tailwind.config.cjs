0c2abd10caf0544119a3af43951de3710624e89f
Sam <samandrewmills@gmail.com>
2022-12-14 14:17:38 +1100

    Added font and custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'primary': '#e82127',
+      'secondary': '#005288',
+      'custom-white': '#FAFAFF',
+      'custom-black': '#0D1321',
+    },
+    fontFamily: { 
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
